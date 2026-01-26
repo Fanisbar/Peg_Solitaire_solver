a688cd5705e28d5fb1484970c09c13c038f84c56
Fanisbar <fanisbar123@gmail.com>
2026-01-26 22:24:03 +0200

    nice way of visualizing

--- solver.py
+++ solver.py
@@ -1,1 +1,15 @@
-print("Need to get to work")
+def visualize_state(state):
+    i=0
+    for char in state:
+        if char=="2":
+            print("  ", end='')
+        elif char=="1":
+            print("+ ", end='')
+        elif char=="0":
+            print("0 ", end='')
+        i+=1
+        if i==7:
+            print("")
+            i=0
+starting_state = "2211122221112211111111110111111111122111222211122"
+visualize_state(starting_state)
