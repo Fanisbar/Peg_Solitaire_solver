bdde9040b1e1ca29f9ea8f41403f4aecafd2864e
Fanisbar <fanisbar123@gmail.com>
2026-01-27 20:04:07 +0200

    expressing and printing states

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 This game is formally known as *peg solitaire*  
 more info: https://en.wikipedia.org/wiki/Peg_solitaire
+
+To play the game, visit: 
--- solver.py
+++ solver.py
@@ -1,15 +1,26 @@
+def make_starting_state(size):
+    state = list()
+    for i in range(1, 3*size-1):
+        row = list()
+        for j in range(1, 3*size-1):
+            # corners
+            if (i<=size-1 or i>2*size-1) and (j<=size-1 or j>2*size-1):
+                row.append(" ")
+            # center
+            elif (i==size+size//2) and (j==size+size//2):
+                row.append("0")
+            # everything else
+            else:
+                row.append("+")
+        state.append(row)
+
+    return state
+
 def visualize_state(state):
-    i=0
-    for char in state:
-        if char=="2":
-            print("  ", end='')
-        elif char=="1":
-            print("+ ", end='')
-        elif char=="0":
-            print("0 ", end='')
-        i+=1
-        if i==7:
-            print("")
-            i=0
-starting_state = "2211122221112211111111110111111111122111222211122"
-visualize_state(starting_state)
+    for row in state:
+        for item in row:
+            print(item+" ", end='')
+        print('')
+
+# test
+visualize_state(make_starting_state(3))
